1825ebc71f0b113731dc3bffc741f90da26757c1
git4sum <pomme426@gmail.com>
2018-03-07 22:42:21 +0900

    Update PTX2PTX.java

--- PTX2PTX.java
+++ PTX2PTX.java
@@ -17,6 +17,64 @@ import java.io.*;
 
 import java.lang.*;
 
+class PTXADDINSTRUCTIONListener extends PTXBaseListener {
+
+  Stack<StringBuilder> out = new Stack<StringBuilder>();
+  PTXParser parser;
+  PTXADDINSTRUCTIONListener(PTXParser parser){
+    out.push(new StringBuilder(""));
+    this.parser = parser;
+  }
+  String labelName;
+  String instructionList;
+  boolean labelExist=false, labelIn=false;;
+  PTXADDINSTRUCTIONListener(PTXParser parser, String labelName, String instructionList){
+    out.push(new StringBuilder(""));
+    this.parser = parser;
+    this.labelName = labelName;
+    this.instructionList = instructionList;
+  }
+
+  @Override  public void exitModDirective(PTXParser.ModDirectiveContext ctx){
+    out.peek().append("\n");
+  }
+  @Override public void enterDirective(PTXParser.DirectiveContext ctx){
+    out.peek().append("\n");
+  }
+  @Override public void exitDirective(PTXParser.DirectiveContext ctx){
+    out.peek().append("\n");
+  }
+  @Override public void enterDeclarationList(PTXParser.DeclarationListContext ctx){
+    out.peek().append("\n");
+  }
+  @Override public void enterDeclaration(PTXParser.DeclarationContext ctx){
+    out.peek().append("\t");
+  }
+  @Override public void exitDeclaration(PTXParser.DeclarationContext ctx){
+    out.peek().append("\n");
+  }
+  @Override public void enterInstructionList(PTXParser.InstructionListContext ctx){
+    out.peek().append("\n");
+  }
+  @Override public void enterInstruction(PTXParser.InstructionContext ctx){
+    if(ctx.getChild(0) instanceof PTXParser.LabelNameContext==false){
+      out.peek().append("\t");
+    } else{
+      out.peek().append("\n");
+    }
+  }
+  @Override public void exitInstruction(PTXParser.InstructionContext ctx){
+    out.peek().append("\n");
+  }
+  @Override public void enterOperandList(PTXParser.OperandListContext ctx){
+    out.peek().append(" ");
+  }
+  @Override public void visitTerminal(TerminalNode node){
+    out.peek().append(node.getText());
+    out.peek().append(" ");
+  }
+}
+
 class PTXADDLABELListener extends PTXBaseListener {
 
   Stack<StringBuilder> out = new Stack<StringBuilder>();
@@ -154,9 +212,7 @@ class PTX2PTXListener extends PTXBaseListener {
 
 public class PTX2PTX {
 
-  //public static void add
-
-   public static String printPTX(String inputPTX, String res) throws IOException {
+  public static String printPTX(String inputPTX, String outputName) throws IOException {
     // Get lexer
     PTXLexer lexer = new PTXLexer(CharStreams.fromString(inputPTX)); //**problem
     //PTXLexer lexer = new PTXLexer(CharStreams.fromFileName(input)); //**problem
@@ -170,8 +226,8 @@ public class PTX2PTX {
     walker.walk(listener, parser.program());// walk from the root of parse tree
 
     // Output file
-    FileOutputStream output = new FileOutputStream(new File("output_"+res));
-    System.out.println("PTX 2 PTX output file name:  output_"+res);
+    FileOutputStream output = new FileOutputStream(new File("output_"+outputName));
+    System.out.println("PTX 2 PTX output file name:  output_"+outputName);
     output.write(listener.out.peek().toString().getBytes());
     output.flush();
     output.close();
@@ -179,7 +235,7 @@ public class PTX2PTX {
     return listener.out.peek().toString();
   }
 
-  public static String addLabelName(String inputPTX, String funcName, String labelName, String res) throws IOException {
+  public static String addLabelName(String inputPTX, String funcName, String labelName, String outputName) throws IOException {
 
     PTXLexer lexer = new PTXLexer(CharStreams.fromString(inputPTX));
     CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -194,8 +250,32 @@ public class PTX2PTX {
     }
 
     // Output file
-    FileOutputStream output = new FileOutputStream(new File("output_"+res));
-    System.out.println("Add basicblock ["+labelName+"] into function ["+funcName+"] output file name:  output_"+res);
+    FileOutputStream output = new FileOutputStream(new File("output_"+outputName));
+    System.out.println("Add basicblock ["+labelName+"] into function ["+funcName+"] output file name:  output_"+outputName);
+    output.write(listener.out.peek().toString().getBytes());
+    output.flush();
+    output.close();
+
+    return listener.out.peek().toString();
+  }
+
+  public static void addInstructionList(String inputPTX, String labelName, String instructionList, String outputName){
+
+    PTXLexer lexer = new PTXLexer(CharStreams.fromString(inputPTX));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    PTXParser parser = new PTXParser(tokens);
+    ParseTreeWalker walker = new ParseTreeWalker();
+    PTXADDINSTRUCTIONListener listener = new PTXADDINSTRUCTIONListener(parser, labelName, instructionList);
+    walker.walk(listener, parser.program());
+
+    if(!listener.labelExist){
+      System.err.println("No Such a Basicblock...");
+      System.exit(1);
+    }
+
+    // Output file
+    FileOutputStream output = new FileOutputStream(new File("output_"+outputName));
+    System.out.println("Add basicblock ["+labelName+"] into function ["+funcName+"] output file name:  output_"+outputName);
     output.write(listener.out.peek().toString().getBytes());
     output.flush();
     output.close();
