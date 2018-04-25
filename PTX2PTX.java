2c1ad156ae6bfb32716123738f29b5ccf39fecc5
git4sum <pomme426@gmail.com>
2018-04-25 19:03:00 +0900

    Update PTX2PTX.java

--- PTX2PTX.java
+++ PTX2PTX.java
@@ -39,7 +39,7 @@ public class PTX2PTX {
 //	    printPTX(tree, args[0]);
 	    insertFunc(".weak .func  (.param .b32 func_retval0) cudaMalloc3(\n.param .b64 cudaMalloc_param_0,\n	.param .b64 cudaMalloc_param_1\n)\n{\n.reg .b32 	%r<2>;\n	st.param.b32	[func_retval0+0], %r1;\n	ret;\n}", 0);
 	    insertInst("BB0_55:\nadd.s32 	%r86, %r1, 193;\nsetp.lt.s32	%rd72, %r86, %r32;", "cudaMalloc3", 0);
-	    modifyOpcode("mov.u32", "cudaMalloc", 1);
+	    modifyOpcode("setp .lt .s32 ", "cudaMalloc", 1);
 	    printPTX(tree, args[0]);
 	}
 
@@ -298,7 +298,6 @@ class PTXfListener extends PTXBaseListener {
   			if(modifyOp){
   				if(ctx.getChild(offset).getChild(0) instanceof PTXParser.OpcodeContext == true){
   					ParserRuleContext child = (ParserRuleContext)ctx.getChild(offset).getChild(0);
-  					System.out.println(child.getText());
   					for(int i = 0; i < child.getChildCount(); i++){
   						child.removeLastChild();
   					}
